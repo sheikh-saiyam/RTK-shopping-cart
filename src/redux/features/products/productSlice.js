7b9de4c9f835cd49c92e996481962cce2104682e
sheikh-saiyam <sheikhsaiyam29@gmail.com>
2025-05-14 17:26:03 +0600

    productSlice Added

---
+++ src/redux/features/products/productSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  products: [],
+};
+
+const productSlice = createSlice({
+  name: "products",
+  initialState,
+  reducers: {},
+});
